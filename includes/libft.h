5863e968dbee2add480902227c2584508369dd32
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-04-18 16:49:10 +0200

    add keyword const in prototype function

--- includes/libft.h
+++ includes/libft.h
@@ -5,9 +5,11 @@
 #include <string.h>
 #include <stdlib.h> // malloc
 
-size_t ft_strlen(char *s);
+size_t ft_strlen(const char *s);
 char *ft_strchr(const char *str, int c);
 char * ft_strrchr(const char *s, int c);
+void * ft_memchr(const void *s, int c, size_t n);
+
 char *ft_strcpy(char *dest, char *src);
 int ft_strcmp(const char *str1, const char *str2);
 #endif
