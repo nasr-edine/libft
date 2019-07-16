56cb9cd337783cf72733dd2d975d94ca6767a220
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-07-16 08:54:08 +0200

    remove lines in strcat

--- ft_strcat.c
+++ ft_strcat.c
@@ -2,12 +2,7 @@
 
 char *ft_strcat(char *s1, const char *s2)
 {
-    size_t len_s1, len_s2;
-
-    len_s1 = strlen(s1);
-    len_s2 = strlen(s2);
-    s1[len_s1 + len_s2] = 0;
-    ft_strcpy(s1+len_s1, s2);
+    ft_strcpy(s1+strlen(s1), s2);
     return s1;
     
 }
