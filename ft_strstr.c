bab6fd11b35277d87ece041166ca71d382516b34
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-07-25 20:09:57 +0200

    remove comments

--- ft_strstr.c
+++ ft_strstr.c
@@ -9,45 +9,18 @@ char *ft_strstr(const char *haystack, const char *needle)
     
     while(haystack[i])
     {
-        // putchar(haystack[i]);
-        // puts("");
         j = 0;
         while(needle[j])
         {
-            
             if (needle[j] != haystack[i + j]) 
-            {
-                // puts(" No ");
                 break;
-            }
-            
-            // putchar(needle[j]);
             j++;
         }     
-        
         if (len == j) 
-        {
-            // puts(" Yes ");
             return (char *)haystack + i;
-        }
-           
-        // puts("");
         i++;
     }
-    
-    if (haystack[0] == '\0' && needle[0] == '\0') {
+    if (haystack[0] == '\0' && needle[0] == '\0') 
         return (char *)haystack;
-    }
-    
     return 0;
-    
 }
-
-
-
-
-        // printf("%zu\n", j);
-        // if (len == j - 1) 
-        // {
-        //     return (char *)haystack + i;
-        // }
