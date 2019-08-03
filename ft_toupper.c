c18f2d33b21329a2076fbba654a2513a7c31b040
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-08-03 15:17:57 +0200

    add toupper

---
+++ ft_toupper.c
@@ -0,0 +1,10 @@
+#include "includes/libft.h"
+
+int ft_toupper(int c)
+{    
+    if (c >= 97 && c <= 122) 
+    {
+        return c - 32;
+    }
+    return c;
+}
--- main.c
+++ main.c
@@ -4,27 +4,16 @@
 
 #include <assert.h>
 #include <string.h>
+#include <ctype.h>
 
 int
 main(int argc, char **argv)
 {
-   char str1[20];
-   char str2[20];
-   int result;
-   int resul;
-
-   //Assigning the value to the string str1
-   strcpy(str1, "hello");
-
-   //Assigning the value to the string str2
-   strcpy(str2, "helLO WORLD");
-
-   //This will compare the first 3 characters
-   result = strncmp(str1, str2, 4);
-   resul = strncmp(str1, str2, 4);
-
-
-printf("%d\n", result);
-printf("%d\n", resul);
+    int a = toupper('a');
+    putchar(a);
+puts("");
+    int b = ft_toupper('a');
+    putchar(b);
+    // printf("%d\n", b);
     return 0;
 }
