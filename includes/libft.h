33a0dfa48ba23bc25ce1ad6df5e0b4ba1032dd2e
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-08-03 16:54:35 +0200

    add isalpha with assert testcase in main

--- includes/libft.h
+++ includes/libft.h
@@ -32,4 +32,6 @@ int ft_strcmp(const char *s1, const char *s2);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 int ft_toupper(int c);
+int ft_tolower(int c);
+int ft_isalpha(int c);
 #endif
--- main.c
+++ main.c
@@ -4,16 +4,50 @@
 
 #include <assert.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h>  
 
 int
 main(int argc, char **argv)
 {
-    int a = toupper('a');
-    putchar(a);
+    int a;
+    // putchar(a);
+        // putc(a, stdout);
+        printf("%d\n", a);
+
 puts("");
-    int b = ft_toupper('a');
-    putchar(b);
-    // printf("%d\n", b);
+    int b;
+    // putchar(b);
+    // putc(b, stdout);
+    
+
+ 
+  
+  
+    int i = 0;
+    int c = -32768;
+
+    while(i < 32767)
+    {
+        a = isalpha(c);
+        b = ft_isalpha(c);
+        assert(a == b);
+        printf("[%c %d %d]\n", c, a, b);
+        c++;
+        i++;
+    }
+
+    // i = 0;
+    // c = 65;
+
+    // while(i < 100)
+    // {
+    //     a = isalpha(c);
+    //     b = ft_isalpha(c);
+    //     assert(a == b);
+    //     printf("[%c %d %d]\n", c, a, b);
+    //     c++;
+    //     i++;
+    // }
+  
     return 0;
 }
